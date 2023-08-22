09498160626932a68518859bad6d7e37549d1b81
Airenas Vaičiūnas <airenass@gmail.com>
2023-08-22 10:32:05 +0300

    Update author

--- intelektika-tts-wp-plugin.php
+++ intelektika-tts-wp-plugin.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name:       Intelektika Text-to-Speech Plugin
 Description:       Convert text of a post to spoken audio using the Intelektika TTS API.
-Version:           1.0.1
-Author:            Airenas Vaiciunas
+Version:           1.0.2
+Author:            Intelektika
 Plugin URI:        https://github.com/Intelektika/tts-wp-plugin/
 Requires at least: 5.2
 Requires PHP:      7.2
